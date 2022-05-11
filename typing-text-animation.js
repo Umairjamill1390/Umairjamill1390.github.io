0d71ba073ebe2827493977922f6ade0925e68af0
Umairjamill1390 <99949345+Umairjamill1390@users.noreply.github.com>
2022-05-11 09:40:26 -0400

    Add files via upload

---
+++ typing-text-animation.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: [
+      "YouTuber",
+      "Developer",
+      "Sketch Artist",
+      "Designer",
+      "Mathematician",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  var typed = new Typed(".typing-2", {
+    strings: [
+      "YouTuber",
+      "Developer",
+      "Sketch Artist",
+      "Designer",
+      "Mathematician",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+});
